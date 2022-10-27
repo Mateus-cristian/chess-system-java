64d7f95b2a836bb99809ac20c1ba53b2ca5013de
mateus-cristian <mateus_cristian@outlook.com>
2022-10-27 17:25:45 -0300

    [feat]Starting implement board and piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,16 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3,5);
+		Board board = new Board(8,8);
 		
-		System.out.println(pos);
+		
+		
 	}
 
 }

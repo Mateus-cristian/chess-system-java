7afa60e60f935255be5720da19a8e39f3e8e17cc
mateus-cristian <mateus_cristian@outlook.com>
2022-11-03 22:04:03 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -71,7 +71,13 @@ public class UI {
 		
 		System.out.println();
 		System.out.println("Turn :" + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());			
+		} else {
+			System.out.println("CHECKMATE!!!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+		}
 		
 		if(chessMatch.getCheck()) {
 			System.out.println("Check!!");
